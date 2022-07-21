b0908c53098697e74291be8208751edcdf5cc50d
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-07-21 11:12:55 -0400

    form validation and js checks

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
 
+let check = function(){
+    const psw= document.getElementById("password");
+    const psw2= document.getElementById("confirmpsw");
+    let error = document.getElementById("psw-error")
+   
+    if(psw.value!=psw2.value){
+        error.style.display="block";
+    }
+    else{
+        error.style.display="none";
+        psw2.classList.add("check");
+    }
+
+}
+let checkReq = function(){
+    let count =0;
+    const password = document.getElementById("password");
+    let error1= document.getElementById("psw-error1");
+    let error2 = document.getElementById("psw-error2");
+    let error3 = document.getElementById("psw-error3");
+    /// Check 1 
+        if(password.value.length < 8){
+            error1.style.color="red";
+            
+        }
+        else{
+            error1.style.color="green";
+            count++;
+        }
+    /// Check 2 
+        error2.style.color="red";
+      for(const e of password.value){
+        if(e.toUpperCase()==e && !isNumeric(e)){
+            error2.style.color="green";
+            count++;
+        }
+      }
+    //// Check 3 
+    error3.style.color="red";
+    for(const e of password.value){
+      if(isNumeric(e)){
+          error3.style.color="green";
+          count++;
+      }
+    }
+///// adding check mark 
+    if(count>=3){
+        password.classList.add("check");
+    }else{
+        password.classList.remove("check");
+    }
+}
+
+/// Checking if number is included
+function isNumeric(val) {
+    return /^-?\d+$/.test(val);
+}
+
 
 
